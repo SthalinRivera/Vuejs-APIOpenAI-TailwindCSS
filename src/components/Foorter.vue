d7cfc12872ac8f7e269cf4aae5f10d905f506c58
STHALIUN RIVERA <STHALIN.11@GMAIL.COM>
2024-08-09 08:52:33 -0500

    se agrego tailwind css

---
+++ src/components/Foorter.vue
@@ -0,0 +1,23 @@
+<template>
+    <a-scene embedded arjs='sourceType: webcam;'>
+      <!-- Definición del marcador -->
+      <a-marker preset="hiro">
+        <a-box position='0 0.5 0' material='color: red;'></a-box>
+      </a-marker>
+      
+      <!-- La cámara AR -->
+      <a-entity camera></a-entity>
+    </a-scene>
+  </template>
+  
+  <script setup>
+    // Puedes agregar lógica aquí si es necesario
+  </script>
+  
+  <style scoped>
+    /* Ajusta el tamaño del canvas para la AR */
+    a-scene {
+      width: 100vw;
+      height: 100vh;
+    }
+  </style>
